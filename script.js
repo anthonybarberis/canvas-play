f60e4f221671ba66d04e308f66b729e444a68735
Anthony Barberis <skamunism@gmail.com>
2019-09-23 22:01:46 -0700

    Added mouse coord tracker

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 window.onload = init;
+var canvas;
+var ctx;
 
 function init() {
-    var canvas = document.querySelector("#myCanvas");
-    var ctx = canvas.getContext('2d');
+    canvas = document.querySelector("#myCanvas");
+    ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
 
     ctx.fillStyle = 'red';
-    ctx.fillRect(0, 0, 100, 100);
+    ctx.fillRect(0, 0, 50, 50);
 
+    canvas.addEventListener('mousemove', function (evt) {
+        mousePos = getMousePos(canvas, evt);
+        var message = " " + mousePos.x + ", " + mousePos.y;
+        document.querySelector("#coords").innerHTML = message;
+    });
+}
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: Math.round(evt.clientX - rect.left),
+        y: Math.round(evt.clientY - rect.top),
+    }
 }
