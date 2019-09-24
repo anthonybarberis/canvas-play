3ac31d27b1fbb486bb01a822ce58eee539a6e71b
Anthony Barberis <skamunism@gmail.com>
2019-09-23 23:00:22 -0700

    Added bouncing animation

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 window.onload = init;
 var canvas;
 var ctx;
+var rect1 = {
+    width: 100,
+    height: 100,
+    posX: Math.random()*20,
+    posY: Math.random()*20,
+    xSpeed: 3,
+    ySpeed: 3,
+    color: 'red'
+}
 
 function init() {
     canvas = document.querySelector("#myCanvas");
     ctx = canvas.getContext('2d');
-    canvas.width = 500;
-    canvas.height = 500;
+    canvas.width = 800;
+    canvas.height = 600;
 
-    ctx.fillStyle = 'red';
-    ctx.fillRect(10, 10, 100, 100);
+    requestAnimationFrame(animateRect);
 
     canvas.addEventListener('mousemove', function (evt) {
         mousePos = getMousePos(canvas, evt);
@@ -24,4 +32,19 @@ function getMousePos(canvas, evt) {
         x: Math.round(evt.clientX - rect.left),
         y: Math.round(evt.clientY - rect.top),
     }
+}
+
+function animateRect() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = rect1.color;
+    ctx.fillRect(rect1.posX, rect1.posY, rect1.width, rect1.height);
+    rect1.posX += rect1.xSpeed;
+    rect1.posY += rect1.ySpeed;
+    if((rect1.posX+rect1.width >= canvas.width) || (rect1.posX <= 0)) {
+        rect1.xSpeed = -rect1.xSpeed;
+    }
+    if((rect1.posY+rect1.height >= canvas.height) || (rect1.posY <= 0)) {
+        rect1.ySpeed = -rect1.ySpeed;
+    }
+    requestAnimationFrame(animateRect);
 }
