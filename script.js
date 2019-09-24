d13b7687060c86a2b08318767ef35443af79d544
Anthony Barberis <skamunism@gmail.com>
2019-09-24 16:42:33 -0700

    Replaced red square with DVD logo

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ window.onload = init;
 var canvas;
 var ctx;
 var rect1 = {
-    width: 100,
+    width: 150,
     height: 100,
-    posX: Math.random()*20,
-    posY: Math.random()*20,
+    posX: Math.random() * 20,
+    posY: Math.random() * 20,
     xSpeed: 3,
     ySpeed: 3,
     color: 'red'
 }
+var dvdLogo = new Image();
+dvdLogo.src = 'dvdlogo.png'
 
 function init() {
     canvas = document.querySelector("#myCanvas");
@@ -17,20 +19,24 @@ function init() {
     canvas.width = 800;
     canvas.height = 600;
 
-    requestAnimationFrame(animateRect);
+    //requestAnimationFrame(animateRect);
+    requestAnimationFrame(animateDVD);
 
     canvas.addEventListener('mousemove', function (evt) {
         mousePos = getMousePos(canvas, evt);
         var message = " " + mousePos.x + ", " + mousePos.y;
         document.querySelector("#coords").innerHTML = message;
     });
-}
 
-function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: Math.round(evt.clientX - rect.left),
-        y: Math.round(evt.clientY - rect.top),
+    ctx.drawImage(dvdLogo, 10, 10, 150, 100);
+
+
+    function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: Math.round(evt.clientX - rect.left),
+            y: Math.round(evt.clientY - rect.top),
+        }
     }
 }
 
@@ -40,11 +46,25 @@ function animateRect() {
     ctx.fillRect(rect1.posX, rect1.posY, rect1.width, rect1.height);
     rect1.posX += rect1.xSpeed;
     rect1.posY += rect1.ySpeed;
-    if((rect1.posX+rect1.width >= canvas.width) || (rect1.posX <= 0)) {
+    if ((rect1.posX + rect1.width >= canvas.width) || (rect1.posX <= 0)) {
         rect1.xSpeed = -rect1.xSpeed;
     }
-    if((rect1.posY+rect1.height >= canvas.height) || (rect1.posY <= 0)) {
+    if ((rect1.posY + rect1.height >= canvas.height) || (rect1.posY <= 0)) {
         rect1.ySpeed = -rect1.ySpeed;
     }
     requestAnimationFrame(animateRect);
+}
+
+function animateDVD() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.drawImage(dvdLogo, rect1.posX, rect1.posY, rect1.width, rect1.height);
+    rect1.posX += rect1.xSpeed;
+    rect1.posY += rect1.ySpeed;
+    if ((rect1.posX + rect1.width >= canvas.width) || (rect1.posX <= 0)) {
+        rect1.xSpeed = -rect1.xSpeed;
+    }
+    if ((rect1.posY + rect1.height >= canvas.height) || (rect1.posY <= 0)) {
+        rect1.ySpeed = -rect1.ySpeed;
+    }
+    requestAnimationFrame(animateDVD);
 }
